7823de8b012cf51dfa140cbbbf9a0c6905a48aef
Dustin Langner <dustin.langner1@gmail.com>
2019-03-05 22:18:36 -0800

    Initial Commit

---
+++ FoodSavior/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodSavior
+//
+//  Created by Dustin Langner on 3/5/19.
+//  Copyright © 2019 Dustin Langner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
