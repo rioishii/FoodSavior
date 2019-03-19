b16c5d8584d0ef1c9a167f963b3d4d55da126539
rioishii <rioishii@uw.edu>
2019-03-19 11:23:15 -0700

    fixed nav swipe

--- FoodSavior/Views/MenuBar.swift
+++ FoodSavior/Views/MenuBar.swift
@@ -59,16 +59,6 @@ class MenuBar: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UIC
     }
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let x = CGFloat(indexPath.item) * frame.width / 2
-        horizontalBarLeftConstraint?.constant = x
-
-        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-             self.layoutIfNeeded()
-        }, completion: nil)
-        UIView.animate(withDuration: 0.75) {
-            self.layoutIfNeeded()
-        }
-        
         recipeViewController!.scrollToMenuIndex(menuIndex: indexPath)
     }
 
