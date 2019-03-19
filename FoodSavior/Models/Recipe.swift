3acb34891d8fc9a8340024120d16a00fdd31e56b
Leandro Solidum <solidum.leandro@gmail.com>
2019-03-18 20:51:20 -0700

    conform Recipe to NSCoding

--- FoodSavior/Models/Recipe.swift
+++ FoodSavior/Models/Recipe.swift
@@ -7,15 +7,16 @@
 //
 
 import UIKit
+import Foundation
 
-class Recipe: NSObject {
+class Recipe: NSObject, NSCoding {
 	let id: Int
 	let name: String
 	let imageUrl: String
 	let readyInMinutes: Int
     var favorite: Bool
 	
-	init(data: [String : Any]) {
+    init(data: [String : Any], favorite: Bool = false) {
 		guard let id = data["id"] as? Int,
 			  let recipeName = data["title"] as? String,
 			  let readyInMinutes = data["readyInMinutes"] as? Int,
@@ -25,7 +26,7 @@ class Recipe: NSObject {
 			self.name = ""
 			self.readyInMinutes = 0
 			self.imageUrl = ""
-            self.favorite = false
+            self.favorite = favorite
 				
 			super.init()
 			return
@@ -35,10 +36,37 @@ class Recipe: NSObject {
 		self.name = recipeName
 		self.readyInMinutes = readyInMinutes
 		self.imageUrl = "https://spoonacular.com/recipeImages/" + imagePath
-        self.favorite = false
+        self.favorite = favorite
 
 		super.init()
 	}
 	
-	
+    // NSCoding Implementation
+    
+    enum Keys: String {
+        case id = "Id"
+        case name = "Name"
+        case imageUrl = "ImageUrl"
+        case readyInMinutes = "ReadyInMinutes"
+        case favorite = "Favorite"
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(id, forKey: Keys.id.rawValue)
+        aCoder.encode(name, forKey: Keys.name.rawValue)
+        aCoder.encode(imageUrl, forKey: Keys.imageUrl.rawValue)
+        aCoder.encode(readyInMinutes, forKey: Keys.readyInMinutes.rawValue)
+        aCoder.encode(favorite, forKey: Keys.favorite.rawValue)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        //add code here
+        let id = aDecoder.decodeObject(forKey: Keys.id.rawValue) as! Int
+        let name = aDecoder.decodeObject(forKey: Keys.name.rawValue) as! String
+        let imageUrl = aDecoder.decodeObject(forKey: Keys.imageUrl.rawValue) as! Int
+        let readyInMinutes = aDecoder.decodeObject(forKey: Keys.readyInMinutes.rawValue) as! String
+        let favorite = aDecoder.decodeObject(forKey: Keys.favorite.rawValue) as! Bool
+        
+        self.init(data: ["id": id, "title": name, "image": imageUrl, "readyInMinutes": readyInMinutes], favorite: favorite)
+    }
 }
